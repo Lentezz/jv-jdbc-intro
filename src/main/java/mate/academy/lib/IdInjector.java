fec0dc9fc3fda36a72154241cb3267d240c76ae8
Lentezz <yaroslav.golovan@gmail.com>
2025-08-01 17:21:53 +0300

    refactoring

--- src/main/java/mate/academy/lib/IdInjector.java
+++ src/main/java/mate/academy/lib/IdInjector.java
@@ -3,14 +3,14 @@ package mate.academy.lib;
 import java.lang.reflect.Field;
 
 public class IdInjector {
-    //TODO add check fot id field data type
-    public static <T extends Number> void injectIdFromDB(Object entity, T idValue) {
+    //TODO make method more abstract using generics and add check fot id field data type
+    public static void injectIdFromDB(Object entity, Long idValue) {
         Field[] fields = entity.getClass().getDeclaredFields();
         for (Field field : fields) {
             if (field.isAnnotationPresent(ID.class)) {
                 field.setAccessible(true);
                 try {
-                    field.set(entity, idValue);
+                    field.setLong(entity, idValue);
                 } catch (IllegalAccessException e) {
                     throw new RuntimeException("Failed to inject ID field into entity: "
                             + entity.getClass().getSimpleName(), e);
